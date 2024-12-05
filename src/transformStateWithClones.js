de34f9b88f9a3ea2d4ac63819d4b0f2d50d50f5a
Flávio Miranda <nascimentoflavio588@hotmail.com>
2024-12-05 19:55:03 -0300

    Add solution task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,35 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrayState = [];
+  let newState = { ...state };
+
+  actions.forEach((action) => {
+    let s = { ...newState };
+
+    switch (action.type) {
+      case 'addProperties':
+        for (const prop in action.extraData) {
+          Object.assign(s, { [prop]: action.extraData[prop] });
+        }
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete s[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        s = {};
+        break;
+    }
+
+    arrayState.push(s);
+    newState = { ...s };
+  });
+
+  return arrayState;
 }
 
 module.exports = transformStateWithClones;
